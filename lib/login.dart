4f6925431cc4466893fddf3a1193d36ad96aff95
shibla256 <shibla256@gmail.com>
2026-06-15 15:54:45 +0530

    changes

--- lib/login.dart
+++ lib/login.dart
@@ -37,53 +37,35 @@ class _LoginState extends State<Login> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      body: Padding(
+        padding: EdgeInsetsGeometry.all(20),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
 
-      body: Padding(padding: EdgeInsetsGeometry.all(20),
-      child: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-
-        children: [
-          TextField(
-
-            controller: username,
-            decoration: InputDecoration(
-              labelText: "username",
-              prefixIcon: Icon(Icons.person_2),
-              border: OutlineInputBorder()
-              
+          children: [
+            TextField(
+              controller: username,
+              decoration: InputDecoration(
+                labelText: "username",
+                prefixIcon: Icon(Icons.person_2),
+                border: OutlineInputBorder(),
+              ),
             ),
+            SizedBox(height: 20),
 
-            
-            
-        
-          ),
-          SizedBox(height: 20,),
-
-          TextField(
-            controller: password,
-            decoration: InputDecoration(
-              labelText: "password",
-
-              prefixIcon: Icon(Icons.lock_clock_outlined),
-
-              border: OutlineInputBorder(),
+            TextField(
+              controller: password,
+              decoration: InputDecoration(
+                labelText: "password",
+                prefixIcon: Icon(Icons.lock_clock_outlined),
+                border: OutlineInputBorder(),
+              ),
             ),
-
-
-
-          ),
-          SizedBox(height: 20,),
-          ElevatedButton(onPressed: login, child: Text("Login")),
-
-          
-
-        
-        ],
-      ),),
-      
-
-
-
+            SizedBox(height: 20),
+            ElevatedButton(onPressed: login, child: Text("Login")),
+          ],
+        ),
+      ),
     );
   }
 }
